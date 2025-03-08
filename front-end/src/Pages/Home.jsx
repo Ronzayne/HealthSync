f1d56408dab1b914e41d2bb1f83acd702c264796
Ronzayne <ronzayne7@gmail.com>
2025-03-08 19:28:38 +0000

    home page

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,5 +1,17 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import Healthquery from "../Components/healthquery";
+import SpecialityMenu from "../Components/SpecialityMenu";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+      <Healthquery />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
