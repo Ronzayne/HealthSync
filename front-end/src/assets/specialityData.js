43fecb18a8e26dc3f4ac7c05847c6c1f07ff3e70
Ronzayne <ronzayne7@gmail.com>
2025-03-08 19:25:24 +0000

    assets

---
+++ front-end/src/assets/specialityData.js
@@ -0,0 +1,33 @@
+import general_physician from "./General_physician.png";
+import gynecologist from "./Gynecologist.png";
+import dermatologist from "./Dermatologist.png";
+import pediatrician from "./Pediatrician.png";
+import neurologist from "./Neurologist.png";
+import gastroenterologist from "./Gastroenterologist.png";
+
+export const specialityData = [
+  {
+    image: general_physician,
+    speciality: "General physician",
+  },
+  {
+    image: gynecologist,
+    speciality: "Gynecologist",
+  },
+  {
+    image: dermatologist,
+    speciality: "Dermatologist",
+  },
+  {
+    image: pediatrician,
+    speciality: "Pediatrician",
+  },
+  {
+    image: neurologist,
+    speciality: "Neurologist",
+  },
+  {
+    image: gastroenterologist,
+    speciality: "Gastroenterologist",
+  },
+];
