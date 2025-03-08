62556e652f3b6e2559f2a9b286e8f7801ba20632
Ronzayne <ronzayne7@gmail.com>
2025-03-08 19:27:14 +0000

    home page

---
+++ front-end/src/Components/Healthquery.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Healthquery = () => {
+  return (
+    <div className="flex flex-col items-center pt-15 pb-35">
+      <p className="text-center text-gray-800  pb-5 leading-tight md:leading-tight lg:leading-tight text-3xl md:text-4xl lg:text-5xl">
+        Health Queries? Interact with our health assistant bot for easy and
+        quick diagnosis
+      </p>
+      <a className="bg-primary px-5 py-3 text-white text-2xl rounded" href="">
+        chat now
+      </a>
+    </div>
+  );
+};
+
+export default Healthquery;
