9e3d3e55bc64db367c69b97f4b8f58d108afef75
Ronzayne <ronzayne7@gmail.com>
2025-03-14 06:14:09 +0000

    doctor asset added

---
+++ front-end/src/assets/doctors.js
@@ -0,0 +1,211 @@
+import doc1 from "./file (2) 1.png";
+import doc2 from "./file (2) 2.png";
+import doc3 from "./file (3) 1.png";
+import doc4 from "./file (3) 2.png";
+import doc5 from "./file (3) 3.png";
+import doc6 from "./file (4) 1.png";
+import doc7 from "./file (4) 2.png";
+import doc8 from "./file (4) 3.png";
+import doc9 from "./file (4) 4.png";
+import doc10 from "./file (5) 1.png";
+import doc11 from "./file (5) 2.png";
+import doc12 from "./file (5) 3.png";
+import doc13 from "./file (5) 4.png";
+
+export const doctors = [
+  {
+    _id: "doc1",
+    name: "Dr. Emmanuel Acquah",
+    image: doc1,
+    speciality: "General physician",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Emmanuel has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc2",
+    name: "Dr. Nana Asante",
+    image: doc2,
+    speciality: "Gynecologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Nana Osei has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc3",
+    name: "Dr. Dennis Agyekum",
+    image: doc3,
+    speciality: "Neurologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Dennis Agyekum has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc4",
+    name: "Dr. Broni Kwakwa",
+    image: doc4,
+    speciality: "Oncologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Broni Kwakwa has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc5",
+    name: "Dr. Emmanuel Acquah",
+    image: doc5,
+    speciality: "Dermatologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Emmanuel has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc6",
+    name: "Dr. Nana Osei",
+    image: doc6,
+    speciality: "Pediatricians",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Nana Osei has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc7",
+    name: "Dr. Kankam Kwaku",
+    image: doc7,
+    speciality: "General physician",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Kankam Kwaku has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc8",
+    name: "Dr. Emmanuel Acquah",
+    image: doc8,
+    speciality: "General physician",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Emmanuel has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc9",
+    name: "Dr. Nana Osei",
+    image: doc9,
+    speciality: "Neurologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Nana Osei has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc10",
+    name: "Dr. Emmanuel Acquah",
+    image: doc10,
+    speciality: "Pediatricians",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Emmanuel has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc11",
+    name: "Dr. Nana Osei",
+    image: doc11,
+    speciality: "Urologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Nana Osei has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc12",
+    name: "Dr. Emmanuel Acquah",
+    image: doc12,
+    speciality: "Gastroenterologist",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Emmanuel has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+  {
+    _id: "doc13",
+    name: "Dr. Emmanuel Acquah",
+    image: doc13,
+    speciality: "General physician",
+    degree: "MBBS",
+    experience: "4 Years",
+    about:
+      "Dr. Emmanuel has a strong commitment to delivering comprehensice medical care",
+    fees: 60,
+    address: {
+      line1: "Ofankor North",
+      line2: "Agya Herbal road",
+    },
+  },
+];
