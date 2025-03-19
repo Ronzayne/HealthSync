e65dbd74d5a58bbd4d86d6577684e54f536bc8b9
Ronzayne <ronzayne7@gmail.com>
2025-03-19 09:14:41 +0000

    Updated AppContext file

--- front-end/src/Context/AppContext.jsx
+++ front-end/src/Context/AppContext.jsx
@@ -1,11 +1,14 @@
 import { createContext } from "react";
 import { doctors } from "../assets/doctors";
+
 export const AppContext = createContext();
 
 const AppCountextProvider = (props) => {
   // whatever is added in the value object can be accessed in any component
+  const currency = "GH₵";
   const value = {
     doctors,
+    currency,
   };
 
   return (
