46ac7c767e6549e4cf5f71df22f9ef5564dff1f6
Ronzayne <ronzayne7@gmail.com>
2025-03-25 21:37:45 +0000

    Created an admin context file

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const AdminContext = createContext();
+
+const AdminContextProvider = (props) => {
+  const [adminToken, setAdminToken] = useState(
+    localStorage.getItem("adminToken") ? localStorage.getItem("adminToken") : ""
+  );
+
+  const [doctors, setDoctors] = useState([]);
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        { headers: { adminToken } }
+      );
+      if (data.success) {
+        setDoctors(data.doctors);
+        console.log(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/change-availability",
+        { docId },
+        { headers: { adminToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAllDoctors(); //to update the doctors state variable
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const value = {
+    adminToken,
+    setAdminToken,
+    backendUrl,
+    doctors,
+    getAllDoctors,
+    changeAvailability,
+  };
+
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
