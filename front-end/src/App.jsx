f97da8ee0e03b937b729f6d2d02ea0a5d638ae90
Ronzayne <ronzayne7@gmail.com>
2025-03-25 21:56:41 +0000

    removed CreateAccount component and added Message component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,20 +8,23 @@ import MyProfile from "./Pages/MyProfile";
 import MyAppointment from "./Pages/MyAppointment";
 import Pharmacies from "./Pages/Pharmacies";
 import NavBar from "./Components/NavBar";
-import CreateAccount from "./Pages/CreateAccount";
+
 import HealthChat from "./Components/HealthChat";
 import Appointments from "./Pages/Appointments";
+import { ToastContainer, toast } from "react-toastify";
+import Message from "./Components/Message";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <ToastContainer />
       <NavBar /> {/*Visible in all the pages*/}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/doctors/:speciality" element={<Doctors />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/create-account" element={<CreateAccount />} />
+
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/my-profile" element={<MyProfile />} />
@@ -30,7 +33,8 @@ const App = () => {
         <Route path="/pharmacies" element={<Pharmacies />} />
         <Route path="/appointments/:docId" element={<Appointments />} />
         <Route path="/healthchat" element={<HealthChat />} />
-        <Route path="/messages" element={<Pharmacies />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/message/:docId" element={<Message />} />
       </Routes>
     </div>
   );
